dfde5fa711c177723fae7b19a89576ae04df084f
Daniel Roe <daniel@roe.dev>
2024-04-23 16:54:18 +0100

    chore: remove logging

--- server/utils/github-api.ts
+++ server/utils/github-api.ts
@@ -3,8 +3,6 @@ import type { H3Event } from 'h3'
 export function useGitHubAPI (event: H3Event) {
   const config = useRuntimeConfig(event)
 
-  console.log('GitHub token:', !!config.githubToken, config.githubToken?.slice(-5))
-  console.log('GitHub token:', !!process.env.NUXT_GITHUB_TOKEN, process.env.NUXT_GITHUB_TOKEN?.slice(-5))
   // Create API client with default values for readability
   return $fetch.create({
     baseURL: `https://api.github.com`,
