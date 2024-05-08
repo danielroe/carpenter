84565a0a9ab7e944ee54cac733427c6fec66859f
Daniel Roe <daniel@roe.dev>
2024-05-08 17:23:34 +0100

    feat: log github api requests in dev mode

--- server/utils/github-api.ts
+++ server/utils/github-api.ts
@@ -1,7 +1,12 @@
 import type { H3Event } from 'h3'
+import type { $Fetch, NitroFetchRequest } from 'nitropack'
 
 export function useGitHubAPI (event: H3Event) {
   const config = useRuntimeConfig(event)
+
+  if (import.meta.dev) {
+    return console.log as $Fetch<unknown, NitroFetchRequest>
+  }
 
   // Create API client with default values for readability
   return $fetch.create({
