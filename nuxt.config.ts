a8fc7d5ff10131289b990b325e7fd516bd4045c9
Daniel Roe <daniel@roe.dev>
2024-08-07 13:09:49 +0100

    chore: bump all dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-08-07',
   devtools: { enabled: true },
   experimental: { noVueServer: true },
   routeRules: {
